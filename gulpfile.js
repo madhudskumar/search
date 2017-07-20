a9321e20382de8f49ba8fc8ee7089d8bfe2c4edb
Madhu Kumar D S <madhudskumar@live.in>
2017-07-21 00:55:10 +0530

    initial commit :: +:base builds

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var gulp = require('gulp'),
+    path = require('path'),
+    data = require('gulp-data'),
+    pug = require('gulp-pug'),
+    prefix = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+/*
+ * Directories here
+ */
+var paths = {
+    public: './dest/',
+    sass: './src/styles/',
+    css: './public/css/'
+};
+
+
+gulp.task('pug', function () {
+    return gulp.src('./src/views/*.pug')
+        .pipe(pug())
+        .on('error', function (err) {
+            process.stderr.write(err.message + '\n');
+            this.emit('end');
+        })
+        .pipe(gulp.dest(paths.public));
+});
+
+/**
+ * Recompile .pug files and live reload the browser
+ */
+gulp.task('rebuild', ['pug'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for pug and sass tasks, then launch the browser-sync Server
+ */
+gulp.task('browser-sync', ['sass', 'pug'], function () {
+    browserSync({
+        server: {
+            baseDir: paths.public
+        },
+        notify: false
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src(paths.sass + '*.scss')
+        .pipe(sass({
+            includePaths: [paths.sass],
+            outputStyle: 'compressed'
+        }))
+        .on('error', sass.logError)
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
+        .pipe(gulp.dest(paths.css))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch .pug files run pug-rebuild then reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch(paths.sass + '**/*.scss', ['sass']);
+    gulp.watch('./src/**/*.pug', ['rebuild']);
+});
+
+gulp.task('build', ['sass', 'pug']);
+
+gulp.task('default', ['browser-sync', 'watch']);
