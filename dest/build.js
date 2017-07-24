a4c06c34c389a5893751791b2f24afefa4fd3fa2
Madhu Kumar D S <madhudskumar@live.in>
2017-07-25 03:06:18 +0530

    final

--- dest/build.js
+++ dest/build.js
@@ -1,11 +1,35 @@
 var doc = document;
 window.onload = function () {
     searchEff();
+    menu();
 };
 function searchEff() {
-    var sForm = document.getElementById('search-form').classList;
-    doc.getElementById('search')
-        .addEventListener('focus', function () { return sForm.add('focus'); });
-    document.getElementById('search')
-        .addEventListener('focusout', function () { return sForm.remove('focus'); });
+    var $sForm = document.getElementById('search-form').classList;
+    var $search = doc.getElementById('search');
+    $search
+        .addEventListener('focus', function () { return $sForm.add('focus'); });
+    $search
+        .addEventListener('focusout', function () { return $sForm.remove('focus'); });
+}
+function menu() {
+    var $left = document.getElementById('left').classList;
+    var $right = document.getElementById('right');
+    var $menu = doc.getElementById('menu');
+    $menu.addEventListener('click', function () {
+        if (!$left.contains('show')) {
+            $left.add('show');
+            $menu.classList.add('margin-left-100');
+            $right.addEventListener('click', function () { return closeMenu(); });
+        }
+        else {
+            closeMenu();
+            $right.removeEventListener('click', function () { }, true);
+        }
+    });
+    function addRightClose() {
+    }
+    function closeMenu() {
+        $left.remove('show');
+        $menu.classList.remove('margin-left-100');
+    }
 }
--- src/script/app.main.ts
+++ src/script/app.main.ts
@@ -2,13 +2,49 @@ const doc = document;
 
 window.onload = () => {
     searchEff();
+    menu();
 };
 
 function searchEff(){
-    const sForm = document.getElementById('search-form').classList;
-    doc.getElementById('search')
-        .addEventListener('focus', () => sForm.add('focus'));
+    const $sForm = document.getElementById('search-form').classList;
+    const $search = doc.getElementById('search');
 
-    document.getElementById('search')
-        .addEventListener('focusout', () => sForm.remove('focus'));
+    $search
+        .addEventListener(
+            'focus',
+            () => $sForm.add('focus'));
+
+    $search
+        .addEventListener(
+            'focusout',
+            () => $sForm.remove('focus')
+        );
+}
+
+function menu(){
+    const $left = document.getElementById('left').classList;
+    const $right = document.getElementById('right');
+    const $menu = doc.getElementById('menu');
+
+    $menu.addEventListener(
+        'click',
+        () => {
+            if(!$left.contains('show')){
+                $left.add('show');
+                $menu.classList.add('margin-left-100');
+                $right.addEventListener('click', () => closeMenu());
+            }else{
+                closeMenu();
+                $right.removeEventListener('click', () => {}, true);
+
+            }
+        });
+
+    function addRightClose() {
+    }
+
+    function closeMenu(){
+        $left.remove('show');
+        $menu.classList.remove('margin-left-100');
+    }
 }
